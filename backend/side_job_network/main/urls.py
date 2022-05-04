57d572bc3e5c868f38df9c73cb85e52af16b9aca
Almas <akzigitovalmas04@gmail.com>
2022-05-04 21:46:10 +0600

    starting

---
+++ backend/side_job_network/main/urls.py
@@ -0,0 +1,10 @@
+from xml.etree.ElementInclude import include
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('about-us', views.about),
+    path('support', views.support),
+    path('sponsorship', views.sponsorship),
+]
---
+++ backend/side_job_network/main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, "main/index.html")
+
+def about(request):
+    return render(request, "main/about.html")
+
+def support(request):
+    return render(request, "main/support.html")
+
+def sponsorship(request):
+    return render(request, "main/sponsorship.html")
+
+
